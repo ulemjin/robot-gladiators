35f5e11e29f8a687d62c4ecb6f727fcb180f0664
Ulemjin Badral <ulemjin@gmail.com>
2021-03-03 10:11:58 -0500

    added player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert ("This is an alert! JS is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+// this created a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
